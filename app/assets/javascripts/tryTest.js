01841ead26453827887be3720e1922886dd46624
ddigioia <derekdigioia@gmail.com>
2014-05-14 13:15:22 -0400

    test show page starting to work

--- app/assets/javascripts/tryTest.js
+++ app/assets/javascripts/tryTest.js
@@ -1,11 +1,18 @@
+var currentTestQuestion;
+var currentTestAnswer;
+
 function tryTest(){
-  console.log(currentTest.id);
-  var currentTestObj = getCurrentTest();
-  console.log(currentTestObj);
-  console.log(currentTestObj.length);
-  for(var i = 0; i < currentTestObj.length; i++){
-    console.log(currentTestObj[i].question_content);
-  }
+  newQuestionAndAnswer();
+  displayTest();
+
+  // for(var i = 0; i < currentTestObj.length; i++){
+  //   currentTestQuestions.push(currentTestObj[i].question_content);
+  //   currentTestAnswers.push(currentTestObj[i].answer);
+  // }
+
+
+  // console.log(currentTestQuestions);
+  // console.log(currentTestAnswers);
 }
 
 
@@ -23,4 +30,29 @@ function getCurrentTest() {
     }
   });
   return currentTestObj;
+}
+
+function newQuestionAndAnswer(){
+  var currentTestObj = getCurrentTest();
+  var shuffledTestEl = _.shuffle(currentTestObj).pop();
+  currentTestAnswer = shuffledTestEl.answer;
+  currentTestQuestion = shuffledTestEl.question_content;
+  console.log(shuffledTestEl);
+  console.log(currentTestAnswer);
+  console.log(currentTestQuestion);
+}
+
+function displayTest(){
+  var testContainer = $('<div class="large-12 columns" ></div>');
+  var testQuestion = $('<p>' + currentTestQuestion + '</p>');
+  var testInput = $('<input type="text" class="createAnswerInput" value="" id="takeTestAnswerInput"/>');
+  var submitButton = $('<button id="takeTestAnswerSubmitButton">Are You Sure?</button>');
+  testQuestion.appendTo(testContainer);
+  testInput.appendTo(testContainer);
+  testContainer.appendTo($('.wrapper'));
+  submitButton.appendTo($('.wrapper'));
+
+  submitButton.click(function(){
+    console.log("WOOOOWZERS");
+  })
 }
